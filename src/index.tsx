dff60eb8f6306ab2c321f780dbb6c9b77ff43aba
Paphon <Paphon_work@hotmail.com>
2023-05-04 21:38:24 +0700

    add react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Nbar from './componet/Nbar';
 import reportWebVitals from './reportWebVitals';
+
+import Home from './pages/Home';
+import About from './pages/About';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+export default function Ro() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+} 
+
 root.render(
   <React.StrictMode>
-    <Nbar />
-    <App />
+    <Ro />
   </React.StrictMode>
 );
 
